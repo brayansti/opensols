aff865c6784c6cb29756e4ebd6f3488c59336517
Brayan Camargo <brayan440@hotmail.com>
2019-11-29 19:32:22 -0500

    Tilde

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h2>
-      Bienvenido, Puedes ingresar en el meú superior o haciendo click <router-link to="/users">aquí</router-link>
+      Bienvenido, Puedes ingresar en el menú superior o haciendo click <router-link to="/users">aquí</router-link>
     </h2>
   </div>
 </template>
